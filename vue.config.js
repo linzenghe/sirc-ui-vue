b8696eb0525def1af85db657ff67147e09dbb63b
linzenghe <1094496868@qq.com>
2021-01-18 23:03:28 +0800

    feat: 配置项

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,76 @@
-const {join} = require('path')
+const utils = require('./build/utils.js')
+const { join } = require('path')
+const aliasConfig = require('./config/alias')
+const { externalMap } = require('./config/index')
 
+const setAlias = (config) => {
+  const { alias } = aliasConfig
+  Object.keys(alias).forEach((key) => {
+    config.resolve.alias.set(key, alias[key])
+  })
+}
 module.exports = {
-    runtimeCompiler: true,
-    productionSourceMap: false,
-    // 修改 pages 入口
-    pages: {
-        index: {
-            entry: 'examples/main.js', // 入口
-            template: 'public/index.html', // 模板
-            filename: 'index.html' // 输出文件
-        }
-    },
-    
-    chainWebpack: (config) => { 
-
+  lintOnSave: !utils.isProduct,
+  runtimeCompiler: true,
+  productionSourceMap: false,
+  pages: {
+    index: {
+      entry: 'examples/main.js',
+      template: 'public/index.html',
+      filename: 'index.html'
     }
+  },
+ 
+  // 扩展 webpack 配置，使 packages 加入编译
+  chainWebpack: (config) => {
+    config.module
+      .rule('js')
+      .include.add(join(process.cwd(), 'src'))
+      .end()
+    // 设置别名
+    setAlias(config)
+    // 关闭利用空余带宽加载文件 提升首页速度
+    config.plugins.delete('preload')
+    config.plugins.delete('prefetch')
+    // 配置别名
+    config.extensions = aliasConfig.resolve
+    config.module
+      .rule('js')
+      .include.add(/packages/)
+      .end()
+      .use('babel')
+      .loader('babel-loader')
+      .tap((options) => {
+        return options
+      })
+    config.when(utils.isProduct, (config) => {
+      // 开启图片压缩
+      config.module
+        .rule('images')
+        .use('image-webpack-loader')
+        .loader('image-webpack-loader')
+        .options({
+          bypassOnDebug: true
+        })
+        .end()
+    })
+  },
+  configureWebpack: (config) => {
+    if (utils.isProduct) {
+      config.externals = externalMap
+    }
+  },
+  devServer: {
+    // 端口号
+    port: 8099,
+    // eslint报错页面会被遮住
+    overlay: {
+      warnings: true,
+      errors: true
+    }
+  },
+  pluginOptions: {
+    lintStyleOnBuild: true,
+    stylelint: {}
+  }
 }
