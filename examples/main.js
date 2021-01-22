770c0cfe5d0e36e6105ae5b4d5bba3c2c69a66ce
linzenghe <linzenghe@fgi.net.cn>
2021-01-22 17:41:51 +0800

    docs: 文档编写

--- examples/main.js
+++ examples/main.js
@@ -1,19 +1,10 @@
-/*
- * @Author: zenghe
- * @Date: 2021-01-14 15:06:07
- * @LastEditTime: 2021-01-20 10:50:26
- * @LastEditors: Do not edit
- * @FilePath: \lib-starter\examples\main.js
- * @Description:
- */
-
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import sui from '@/index' // 引入组件库
-Vue.use(sui)
+import sircUI from '@/index' // 引入组件库
+Vue.use(sircUI)
 Vue.config.productionTip = false
 
 new Vue({
--- src/index.js
+++ src/index.js
@@ -1,17 +1,153 @@
-import { Button } from 'ant-design-vue'
+
+import {
+  Affix,
+  AutoComplete,
+  Alert,
+  // Avatar,
+  BackTop,
+  Badge,
+  Breadcrumb,
+  Button,
+  // Calendar,
+  Card,
+  Collapse,
+  // Carousel,
+  Cascader,
+  Checkbox,
+  Col,
+  DatePicker,
+  Divider,
+  Dropdown,
+  Form,
+  FormModel,
+  Icon,
+  InputNumber,
+  Layout,
+  List,
+  // LocaleProvider,
+  Menu,
+  // Mentions,
+  Modal,
+  Pagination,
+  Popconfirm,
+  Popover,
+  // Progress,
+  Radio,
+  Rate,
+  Row,
+  Select,
+  // Slider,
+  Spin,
+  // Statistic,
+  Steps,
+  Switch,
+  Table,
+  Transfer,
+  Tree,
+  TreeSelect,
+  Tabs,
+  Tag,
+  TimePicker,
+  Timeline,
+  Tooltip,
+  Upload,
+  Drawer,
+  Skeleton,
+  // Comment,
+  ConfigProvider,
+  Empty,
+  Result,
+  Descriptions,
+  // PageHeader,
+  message,
+  notification
+} from 'ant-design-vue'
 import Input from '../packages/Input'
 import 'ant-design-vue/dist/antd.css'
 
 import { version } from '../package.json'
-const components = [Button, Input]
+const components = [
+  Affix,
+  AutoComplete,
+  Alert,
+  // Avatar,
+  BackTop,
+  Badge,
+  Breadcrumb,
+  Button,
+  // Calendar,
+  Card,
+  Collapse,
+  // Carousel,
+  Cascader,
+  Checkbox,
+  Col,
+  DatePicker,
+  Divider,
+  Dropdown,
+  Form,
+  FormModel,
+  Icon,
+  Input,
+  InputNumber,
+  Layout,
+  List,
+  // LocaleProvider,
+  Menu,
+  // Mentions,
+  Modal,
+  Pagination,
+  Popconfirm,
+  Popover,
+  // Progress,
+  Radio,
+  Rate,
+  Row,
+  Select,
+  // Slider,
+  Spin,
+  // Statistic,
+  Steps,
+  Switch,
+  Table,
+  Transfer,
+  Tree,
+  TreeSelect,
+  Tabs,
+  Tag,
+  TimePicker,
+  Timeline,
+  Tooltip,
+  Upload,
+  Drawer,
+  Skeleton,
+  // Comment,
+  ConfigProvider,
+  Empty,
+  Result,
+  Descriptions,
+  // PageHeader,
+  message,
+  notification
+]
 const install = function(Vue) {
   // 判断是否安装
   if (install.installed) {
     return
   }
+  // 加载组件
   components.forEach((component) => {
     Vue.component(component.name, component)
   })
+  // vue 原型上添加方法
+  Vue.prototype.$message = message
+  Vue.prototype.$notification = notification
+  Vue.prototype.$info = Modal.info
+  Vue.prototype.$success = Modal.success
+  Vue.prototype.$error = Modal.error
+  Vue.prototype.$warning = Modal.warning
+  Vue.prototype.$confirm = Modal.confirm
+  Vue.prototype.$destroyAll = Modal.destroyAll
 }
 
 if (typeof window !== 'undefined' && window.Vue) {
